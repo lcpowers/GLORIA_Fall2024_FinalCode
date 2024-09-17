bb56db7ec671dfa2407477eec266548092ec1a0f
lcpowers <lipo6619@colorado.edu>
2024-09-17 17:00:14 -0600

    Something fishy is happening

---
+++ Scripts/1_DataPrep/TemperatureData/1_CompileRawTemp_FindGaps.R
@@ -0,0 +1,221 @@
+#### Compile soil temperature data then find and visualize gaps in the data ####
+rm(list=ls())
+
+library(tidyverse)
+library(lubridate)
+library(viridis)
+library(visdat)
+
+# ####### Compile T Data ########
+# # First compile all T data and save
+# # Read in GNP soil temp data
+# gnp_temp_files <- list.files("./Data/climate/GlacierNational-SoilTemp/",pattern = ".csv",recursive = T)
+# gnp_temp_data <- NULL
+# 
+# for(i in gnp_temp_files){
+# 
+#   print(which(gnp_temp_files==i))
+#   tmp <- read_csv(paste0("./Data/climate/GlacierNational-SoilTemp/",i))
+#   tmp$Date <- as.Date(x = tmp$Date,format = "%m/%d/%Y")
+#   tmp <- tmp %>%
+#     separate(Site,c("summit","aspect"),remove=F) %>%
+#     separate(Date,c("year","month","day"),remove=F) %>%
+#     mutate(park="GNP") %>%
+#     select(park,summit,aspect,date=Date,year,month,day,time=Time,tempC=`Temp C`)
+# 
+#   gnp_temp_data <- rbind(gnp_temp_data,tmp)
+#   rm(tmp)
+# }
+# rm(i)
+# # If using the same data set, this df should be 1766756 lines long
+# 
+# # Write compiled GNP temp data rather than looping to compile every time
+# write_csv(gnp_temp_data,"./Data/IntermediateData/GNP_rawT.csv")
+
+gnp_temp_data <- read_csv("./Data/IntermediateData/GNP_rawT.csv")
+
+# Create plot column -- identical to aspect but makes GNP column names match other temp data.frame
+gnp_temp_data$plot <- gnp_temp_data$aspect
+###### End Glacier data ######
+
+# Read in temp data for GRSD, ROMO, Yell, and PECO
+temp_data <- read_csv("./Data/climate/NPS_IMD_GLORIA_SoilTemperature_2288176_DataPackage_UPDATED_Pecos/NPS_IMD_GLORIA_SoilTemperature_2288176_Raw-dataset.csv") %>% 
+  mutate(aspect=str_sub(Plot,1,1)) %>%      # Create an aspect column using substring of the plot column
+  select(park=Park,summit=Summit,plot=Plot,aspect,datetime=DateTime,tempC=Value) %>% # rename columns to be all lower case
+  separate(datetime,c('date','time'),sep=" ") # separate components (date and time) of datetime column
+temp_data$date <- as.Date(temp_data$date) # Convert date column to date format
+
+# Separate Y, M, D columns
+temp_data <- separate(temp_data,date,c("year","month","day"),remove=F)
+
+# Change temp data park codes to match veg data park codes
+temp_data$park[temp_data$park=="GRSA"] <- "GSD"
+temp_data$park[temp_data$park=="ROMO"] <- "RMN"
+temp_data$park[temp_data$park=="YELL"] <- "YNP"
+temp_data$park[temp_data$park=="USFS_PEC"] <- "PEC"
+
+all_raw_temp_data <- rbind(temp_data,gnp_temp_data)
+all_raw_temp_data$day=as.numeric(all_raw_temp_data$day)
+all_raw_temp_data$month=as.numeric(all_raw_temp_data$month)
+all_raw_temp_data$year=as.numeric(all_raw_temp_data$year)
+
+# Get rid of extra dfs to keep env clean
+rm(temp_data,gnp_temp_data,gnp_temp_files)
+
+# Write csv of compiled raw temp data
+# write_csv(all_raw_temp_data,"./Data/IntermediateData/rawTemp_all.csv")
+
+####### Find temperature gaps ######
+all_raw_temp_data$park <- as.factor(all_raw_temp_data$park)
+all_raw_temp_data$summit <- as.factor(all_raw_temp_data$summit)
+all_raw_temp_data$plot <- as.factor(all_raw_temp_data$plot)
+all_raw_temp_data$aspect <- as.factor(all_raw_temp_data$aspect)
+all_raw_temp_data$date <- as.Date(all_raw_temp_data$date)
+
+# Get list of unique summits
+summits <- unique(as.character(all_raw_temp_data$summit))
+
+# Initiate dataframe for all dates
+dailyT_alldates <- NULL
+
+for(i in summits){
+  # find mean T by day for individual aspects on summit i.
+  # Use all columns that we want to keep as grouping variable
+  tmp_summit <- filter(all_raw_temp_data,summit == i) %>% 
+    group_by(park,summit,aspect,date,year,month,day,) %>% 
+    dplyr::summarise(meanT=mean(tempC,na.rm=T),
+              n_obs = n(),
+              .groups = "drop")
+  
+  # Get all unique aspects. Should be N,S,W,E
+  aspects = unique(tmp_summit$aspect)
+  
+  for(j in aspects){
+    
+    # Filter summit data for aspect j
+    tmp_aspect <- tmp_summit %>% 
+      filter(aspect==j)
+    
+    # Create dataframe of complete date range by day
+    tmp_date_range = data.frame(missing_dates=seq.Date(from = min(tmp_aspect$date),
+                                                       to = max(tmp_aspect$date),
+                                                       by = 1),
+                                park=tmp_aspect$park[1],summit=i,aspect=j) %>% 
+      mutate(year=year(missing_dates),
+             month=month(missing_dates),
+             day=day(missing_dates))
+    
+    # Merge full date range with main dataframe, keeping all possible dates 
+    tmp_full <- merge(tmp_aspect,tmp_date_range,
+                      by.x=c('date','park','summit','aspect','year','month','day'),
+                      by.y=c('missing_dates','park','summit','aspect','year','month','day'),
+                      all.y = T)
+    tmp_full$n_obs[is.na(tmp_full$meanT)]=0
+    
+    dailyT_alldates = rbind(dailyT_alldates,tmp_full)  
+  }
+  
+  # assign(paste0(i,"_alldates"),tmp_output)
+  
+  rm(tmp_summit,tmp_date_range,tmp_aspect,tmp_full)
+
+}
+# write_csv(dailyT_alldates,"Data/IntermediateData/dailyT_unfilled.csv")
+
+romo <- filter(dailyT_alldates,park=="RMN"&aspect!="C") # 63335 rows (65941 rows with aspect=="C")
+yell <- filter(dailyT_alldates,park=="YNP") # 52547 rows
+grsd <- filter(dailyT_alldates,park=="GSD") # 61413 rows
+glac <- filter(dailyT_alldates,park=="GNP"&aspect!="HSP"&!aspect%in%c("NW","SW","SE")) # 72946 rows (77278 rows with NW, SW, SE) (83821 rows with aspect=="HSP")
+peco <- filter(dailyT_alldates,park=="PEC")
+
+### Rocky Mountain ###
+# Number of missing days per year
+romo_table <- table(romo$year, as.character(romo$summit),as.character(romo$aspect),romo$n_obs) %>% as.data.frame() %>% 
+  filter(Var4==0) %>% 
+  select(-Var4)
+colnames(romo_table) <- c("year","summit","aspect","n_missing")
+romo_wide_table <- pivot_wider(data = romo_table,
+                               id_cols = c(year,aspect),
+                               names_from = summit,
+                               values_from = n_missing,
+                               values_fill = 0)
+
+ggplot(subset(romo,summit=="VQS"),aes(x=date,y=n_obs,color=n_obs))+
+  geom_point(alpha=1,size=0.01)+
+  facet_wrap(~summit+aspect)+
+  theme_classic(base_size = 14)+
+  ggtitle("Rocky Mountain")
+
+##### Yellowstone #####
+yell_table <- table(yell$year,as.character(yell$summit),as.character(yell$aspect),yell$n_obs) %>% as.data.frame() %>% 
+  filter(Var4==0) %>% 
+  select(-Var4)
+colnames(yell_table) <- c("year","summit","aspect","n_missing")
+yell_wide_table <- pivot_wider(data = yell_table,
+                               id_cols = c(year,aspect),
+                               names_from = summit,
+                               values_from = n_missing,
+                               values_fill = 0)
+
+ggplot(yell, aes(x=date,y=n_obs,color=n_obs))+
+  geom_point(alpha=0.75)+
+  facet_wrap(~summit+aspect)+
+  theme_classic(base_size = 7)+
+  ggtitle("Yellowstone")+
+  scale_color_viridis(direction=-1)
+
+##### Great Sand Dunes #####
+grsd_table <- table(grsd$year,as.character(grsd$summit),as.character(grsd$aspect),grsd$n_obs) %>% as.data.frame() %>% 
+  filter(Var4==0) %>% 
+  select(-Var4) %>% 
+  filter(!Var3%in%c("HSP","NW","SW","SE","C"))
+colnames(grsd_table) <- c("year","summit","aspect","n_missing")
+grsd_wide_table <- pivot_wider(data = grsd_table,
+            id_cols = c(year,aspect),
+            names_from = summit,
+            values_from = n_missing,
+            values_fill = 0)
+
+ggplot(grsd, aes(x=date,y=n_obs,color=n_obs))+
+  geom_point(alpha=0.75)+
+  facet_wrap(~aspect+summit)+
+  theme_classic(base_size = 14)+
+  ggtitle("Great Sand Dunes")+
+  scale_color_viridis(direction=-1)
+
+###### Glacier #######
+glac_table <- table(glac$year,as.character(glac$summit),as.character(glac$aspect),glac$n_obs) %>% as.data.frame() %>% 
+  filter(Var4==0) %>% 
+  select(-Var4)
+colnames(glac_table) <- c("year","summit","aspect","n_missing")
+glac_wide_table <- pivot_wider(data =glac_table,
+                               id_cols = c(year,summit),
+                               names_from = aspect,
+                               values_from = n_missing,
+                               values_fill = NULL)
+
+ggplot(glac, aes(x=date,y=n_obs,color=n_obs))+
+  geom_point(alpha=0.75)+
+  facet_wrap(~summit+aspect)+
+  theme_classic(base_size = 7)+
+  ggtitle("Glacier")+
+  scale_color_viridis(direction=-1)
+
+###### Pecos ######
+peco_table <- table(peco$year,as.character(peco$summit),as.character(peco$aspect),peco$n_obs) %>% as.data.frame() %>% 
+  filter(Var4==0) %>% 
+  select(-Var4)
+colnames(peco_table) <- c("year","summit","aspect","n_missing")
+peco_wide_table <- pivot_wider(data = peco_table,
+                               id_cols = c(year,summit),
+                               names_from = aspect,
+                               values_from = n_missing,
+                               values_fill = NULL)
+
+ggplot(peco, aes(x=date,y=n_obs,color=n_obs))+
+  geom_point(alpha=0.75)+
+  facet_wrap(~summit+aspect)+
+  theme_classic(base_size = 7)+
+  ggtitle("pecos")+
+  scale_color_viridis(direction=-1)
+#######
---
+++ Scripts/1_DataPrep/TemperatureData/2_WaterBalanceModel_Thoma.R
@@ -0,0 +1,133 @@
+####### Water Balance code ##########
+
+library(WaterBalance) # Will need to access a zip file provided by D. Thoma to install this package
+library(geosphere)
+library(raster)
+library(lubridate)
+library(tidyverse)
+library(readxl)
+
+## Two pound signs are lines from the original script that I commented out to replace with my own lines (CP)
+
+############################################################# USER INPUTS ##################################################################### 
+rm(list=ls())
+
+##### Read in prism data #####
+prism_files <- list.files("./Data/climate/prism/", pattern = "1990.csv",full.names = T)
+
+Historical <- NULL
+
+for(i in prism_files){
+  
+  summiti <- str_sub(string = basename(i),start = 1,end = 3)
+  df <- read_csv(i,skip=10) %>%
+    mutate(summit=summiti) %>%
+    select(summit,date=Date,ppt="ppt (mm)",
+           tmean="tmean (degrees C)",tmax="tmax (degrees C)",tmin="tmin (degrees C)") 
+  
+  Historical <- rbind(Historical,df)
+  
+  rm(df,summiti)
+}
+rm(prism_files,i)
+
+# Site characteristics 
+# sites = read.csv("C:/Users/adillon/Documents/RSS/CONG/WB/CONG_site_characteristics.csv") #CSV file containing properties for all sites
+
+### Read in David's site info csv
+siteinfo = read_csv("./Data/siteInfo/summitcoordinates.csv") %>% select(summit,elev,startdate,enddate)
+wb_sites = read_xlsx("./Data/climate/daymet/sites.xlsx") %>% 
+  mutate(park=str_sub(site,1,3),
+         summit=str_sub(site,5,7),
+         aspectDeg=aspect,
+         aspect=str_sub(site,9,9),
+         WB_site=paste(summit,aspect,sep="_")) 
+
+wb_sites = merge(wb_sites,siteinfo,by="summit")
+
+n<-nrow(wb_sites)
+#Threshold temperature (deg C) for growing degree-days calculation
+T.Base = 0 
+
+#Method for PET calculation 
+Method = "Oudin"  #Oudin is default method for daily PRISM and MACA data (containing only Tmax, Tmin, and Date). 
+
+#Date format
+DateFormat = "%m/%d/%Y"
+
+############################################################ END USER INPUTS ###################################################################
+
+######################################################### CALCULATE WB VARIABLES ################################################################
+AllDailyWB<-list()
+
+  for(i in 1:nrow(wb_sites)){
+
+    ID = wb_sites$WB_site[i]
+    
+    DailyWB<-filter(Historical,summit==str_sub(ID,1,3))
+    
+    park = wb_sites$park[wb_sites$WB_site==ID]
+    Lat = wb_sites$latitude[i] 
+    Lon = wb_sites$longitude[i] 
+    Elev = wb_sites$elev[i] ## Elevation -> elev
+    Aspect = wb_sites$aspectDeg[i] ## Aspect -> aspectDeg
+    Slope = wb_sites$slope[i] ## Could bring this in but maybe don't need it
+    SWC.Max = 50 ## From excel sheet
+    Snowpack.Init = 0 ## optional in get_melt() function with default = 0
+    Soil.Init = 0 ## wb_sites$Soil.Init[i] ## Soil initial conditions. Not sure what this is or how David determines this
+    Shade.Coeff = 1 ## wb_sites$Shade.Coeff[i] No shade -- alpine
+    #Calculate daily water balance variables 
+    
+    DailyWB$park = park
+    DailyWB$ID = ID
+    DailyWB$doy <- yday(DailyWB$date) ## Date to date
+    DailyWB$daylength = get_daylength(DailyWB$date, Lat)
+    
+    DailyWB$jtemp = as.numeric(get_jtemp(Lon, Lat))
+    # print(mean(DailyWB$jtemp))
+    
+    DailyWB$F = get_freeze(DailyWB$jtemp, DailyWB$tmean)
+    
+    DailyWB$RAIN = get_rain(DailyWB$ppt, DailyWB$F)
+    DailyWB$SNOW = get_snow(DailyWB$ppt, DailyWB$F)
+    DailyWB$MELT = get_melt(tmean = DailyWB$tmean, jtemp = DailyWB$jtemp, 
+                            hock=4, snow = DailyWB$SNOW, sp.0 = Snowpack.Init) ## Snowpack init?? ##
+    
+    # print(c(mean(DailyWB$jtemp),mean(DailyWB$SNOW),mean(DailyWB$MELT)))
+    
+    DailyWB$PACK = get_snowpack(DailyWB$jtemp, DailyWB$SNOW, DailyWB$MELT) # Daily snowpack depth
+    DailyWB$W = DailyWB$MELT + DailyWB$RAIN # Daily soil water inputs
+    
+    if(Method == "Hamon"){
+      DailyWB$PET = ET_Hamon_daily(DailyWB)
+    } else {
+      if(Method == "Penman-Monteith"){
+        DailyWB$PET = ET_PenmanMonteith_daily(DailyWB)
+      } else {
+        if(Method == "Oudin"){
+          DailyWB$PET = get_OudinPET(DailyWB$doy, Lat, DailyWB$PACK, DailyWB$tmean, Slope, Aspect, Shade.Coeff)
+        } else {
+          print("Error - PET method not found")
+        }
+      }
+    }
+    
+    DailyWB$PET = modify_PET(DailyWB$PET, Slope, Aspect, Lat, Shade.Coeff)
+    DailyWB$W_PET = DailyWB$W - DailyWB$PET
+    DailyWB$SOIL = get_soil(DailyWB$W, Soil.Init, DailyWB$PET, DailyWB$W_PET, SWC.Max) # Soil water content
+    DailyWB$DSOIL = diff(c(Soil.Init, DailyWB$SOIL)) # Starting soil conditions - soil water content??
+    DailyWB$AET = get_AET(DailyWB$W, DailyWB$PET, DailyWB$SOIL, Soil.Init) 
+    DailyWB$W_ET_DSOIL = DailyWB$W - DailyWB$AET - DailyWB$DSOIL
+    DailyWB$D = DailyWB$PET - DailyWB$AET
+    DailyWB$GDD = get_GDD(DailyWB$tmean, T.Base)
+    AllDailyWB[[i]] = DailyWB
+    rm(DailyWB)
+  }
+WBData<-do.call(rbind,AllDailyWB)
+write_csv(WBData,"./Data/IntermediateData/dailyWBdata.csv")
+######################################################### END WB VARIABLE CALCULATIONS ################################################################
+
+
+
+
+
